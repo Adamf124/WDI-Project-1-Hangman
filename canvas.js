146a54561a9866d2c4475ffc1e7d8c47957aecc1
Adamf124 <adam.ferguson124@gmail.com>
2018-05-23 14:16:21 -0400

    new game is a button created ul

---
+++ canvas.js
@@ -0,0 +1,12 @@
+window.onload = function hangman () {
+
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
+console.log(alphabet)
+alphabet.toString
+console.log(alphabet)
+
+var alphalist = document.createElement('ul')
+alphaButtons = document.getElementById('buttons').appendChild(alphalist);
+
+
+}
