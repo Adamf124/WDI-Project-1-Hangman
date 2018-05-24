3ab92c5dbdd1c225f3692560ee0679b25c2815c4
Adamf124 <adam.ferguson124@gmail.com>
2018-05-24 12:54:53 -0400

    Declared variables

---
+++ canvas.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  let letter = '';
+  let currentword = "";
+  let lives = 7;
+  const secretwords = ['dog','cat','horse','zebra','giraffe','lions','tigers','bears','monkey','snake','wolf','fox'];
+  let hiddenwords = [];
+  
+}
